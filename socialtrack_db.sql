d8b239fa121fd77a42e0512f55ca3b3b15dddf83
LoryBug <72506393+LoryBug@users.noreply.github.com>
2023-01-23 16:00:07 +0100

    add queries

---
+++ socialtrack_db.sql
@@ -0,0 +1,239 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 23, 2023 at 03:01 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `socialtrack_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `CommentID` char(10) NOT NULL,
+  `Text` char(200) NOT NULL,
+  `Timestamp` date NOT NULL,
+  `PostID` char(10) NOT NULL,
+  `Username` char(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `follow`
+--
+
+CREATE TABLE `follow` (
+  `FOL_Username` char(50) NOT NULL,
+  `Username` char(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifica`
+--
+
+CREATE TABLE `notifica` (
+  `NotID` char(10) NOT NULL,
+  `CommentID` char(10) DEFAULT NULL,
+  `Type` char(100) NOT NULL,
+  `ReviewID` char(10) DEFAULT NULL,
+  `Text` char(200) NOT NULL,
+  `Checked` char(1) NOT NULL,
+  `Username` char(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE `post` (
+  `PostID` char(10) NOT NULL,
+  `Timestamp` date NOT NULL,
+  `Text` char(200) NOT NULL,
+  `Image` char(100) DEFAULT NULL,
+  `Username` char(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
+
+CREATE TABLE `review` (
+  `ReviewID` char(10) NOT NULL,
+  `NotID` char(10) DEFAULT NULL,
+  `Text` char(200) NOT NULL,
+  `Timestamp` date NOT NULL,
+  `Voto` decimal(2,0) NOT NULL,
+  `TrackID` decimal(10,0) NOT NULL,
+  `Username` char(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `track`
+--
+
+CREATE TABLE `track` (
+  `TrackID` decimal(10,0) NOT NULL,
+  `Text` char(200) NOT NULL,
+  `Type` char(30) NOT NULL,
+  `Timestamp` date NOT NULL,
+  `Length` float NOT NULL,
+  `Region` char(40) DEFAULT NULL,
+  `FileGPX` char(200) NOT NULL,
+  `Image` char(100) DEFAULT NULL,
+  `Username` char(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `Username` char(50) NOT NULL,
+  `Email` char(50) NOT NULL,
+  `Password` char(30) NOT NULL,
+  `nFollow` decimal(10,0) NOT NULL,
+  `nFollower` decimal(10,0) NOT NULL,
+  `ProfileImg` char(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`Username`, `Email`, `Password`, `nFollow`, `nFollower`, `ProfileImg`) VALUES
+('LoryBart', 'lory.leoni@gmail.com', 'ciao', '10', '10', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.begingroup.net%2Fessere-bart-simpson%2F&psig=A');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`CommentID`),
+  ADD KEY `fk_Comm_Post` (`PostID`),
+  ADD KEY `fk_Comm_User` (`Username`);
+
+--
+-- Indexes for table `follow`
+--
+ALTER TABLE `follow`
+  ADD PRIMARY KEY (`FOL_Username`,`Username`),
+  ADD KEY `fk_Follow_User_1` (`Username`);
+
+--
+-- Indexes for table `notifica`
+--
+ALTER TABLE `notifica`
+  ADD PRIMARY KEY (`NotID`),
+  ADD KEY `fk_Notif_User` (`Username`),
+  ADD KEY `fk_Notif_Comment` (`CommentID`),
+  ADD KEY `fk_Notif_Review` (`ReviewID`);
+
+--
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`PostID`),
+  ADD KEY `fk_Post_User` (`Username`);
+
+--
+-- Indexes for table `review`
+--
+ALTER TABLE `review`
+  ADD PRIMARY KEY (`ReviewID`),
+  ADD KEY `fk_Rev_Track` (`TrackID`),
+  ADD KEY `fk_Rev_User` (`Username`);
+
+--
+-- Indexes for table `track`
+--
+ALTER TABLE `track`
+  ADD PRIMARY KEY (`TrackID`),
+  ADD KEY `fk_Track_User` (`Username`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`Username`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `fk_Comm_Post` FOREIGN KEY (`PostID`) REFERENCES `post` (`PostID`),
+  ADD CONSTRAINT `fk_Comm_User` FOREIGN KEY (`Username`) REFERENCES `user` (`Username`);
+
+--
+-- Constraints for table `follow`
+--
+ALTER TABLE `follow`
+  ADD CONSTRAINT `fk_Follow_User` FOREIGN KEY (`FOL_Username`) REFERENCES `user` (`Username`),
+  ADD CONSTRAINT `fk_Follow_User_1` FOREIGN KEY (`Username`) REFERENCES `user` (`Username`);
+
+--
+-- Constraints for table `notifica`
+--
+ALTER TABLE `notifica`
+  ADD CONSTRAINT `fk_Notif_Comment` FOREIGN KEY (`CommentID`) REFERENCES `comment` (`CommentID`),
+  ADD CONSTRAINT `fk_Notif_Review` FOREIGN KEY (`ReviewID`) REFERENCES `review` (`ReviewID`),
+  ADD CONSTRAINT `fk_Notif_User` FOREIGN KEY (`Username`) REFERENCES `user` (`Username`);
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `fk_Post_User` FOREIGN KEY (`Username`) REFERENCES `user` (`Username`);
+
+--
+-- Constraints for table `review`
+--
+ALTER TABLE `review`
+  ADD CONSTRAINT `fk_Rev_Track` FOREIGN KEY (`TrackID`) REFERENCES `track` (`TrackID`),
+  ADD CONSTRAINT `fk_Rev_User` FOREIGN KEY (`Username`) REFERENCES `user` (`Username`);
+
+--
+-- Constraints for table `track`
+--
+ALTER TABLE `track`
+  ADD CONSTRAINT `fk_Track_User` FOREIGN KEY (`Username`) REFERENCES `user` (`Username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

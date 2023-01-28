d065ab0c7b8b955738290f057724e0054e6637df
LoryBug <72506393+LoryBug@users.noreply.github.com>
2023-01-28 10:40:20 +0100

    fix bug

--- index.php
+++ index.php
@@ -5,13 +5,13 @@ if (isset($_POST["textAreaPost"])) {
     //var_dump($_POST["textAreaPost"]);
     $nPostID = $dbh->getPostNewID()[0]["COUNT(PostID)+1"];
     $datetimePost = date("Y/m/d H:i:s");
-    $dbh->insertNewPost("$nPostID", "$datetimePost", "$_POST[textAreaPost]", "upload/$_POST[ImgInput]", "GiammaC");
+    $dbh->insertNewPost("$nPostID", "$datetimePost", "$_POST[textAreaPost]", "upload/$_POST[ImgInput]", $_SESSION["username"]);
 }
 if (isset($_POST["CommentInput"])) {
     //var_dump($_POST["CommentInput"]);
     $nCommentID = $dbh->getCommentNewID()[0]["COUNT(CommentID)+1"];
     $datetimeComment = date("Y/m/d H:i:s");
-    $dbh->insertNewComment("$nCommentID", "$_POST[CommentInput]", "$datetimeComment", "$_POST[postID]", "GiammaC");
+    $dbh->insertNewComment("$nCommentID", "$_POST[CommentInput]", "$datetimeComment", "$_POST[postID]",$_SESSION["username"]);
 }
 
 $templateParams["titolo"] = "Socialtrack - Home";

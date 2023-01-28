2db7600edb8a7df9398e235f30701d46b3c81ee4
LoryBug <72506393+LoryBug@users.noreply.github.com>
2023-01-28 18:46:03 +0100

    add file

--- profile.php
+++ profile.php
@@ -7,8 +7,8 @@ $templateParams["username"] = $_SESSION['username'];
 $templateParams["imgProfile"] = $dbh->getUserImg($_SESSION['username'])[0]["ProfileImg"];
 $templateParams["email"] = $dbh->getUserEmail($_SESSION['username'])[0]["Email"];
 
-$templateParams["listaTrack"] = "lista-track.php";
-$templateParams["listaPost"] = "lista-post.php";
+$templateParams["listaTrack"] = "lista-user-track.php";
+$templateParams["listaPost"] = "lista-user-post.php";
 $templateParams["followers"] = "lista-follower.php";
 $templateParams["following"] = "lista-following.php";
 $templateParams["notifiche"] = "lista-notifiche.php";

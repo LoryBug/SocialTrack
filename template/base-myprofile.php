95fce2d95c41242b0a9c9dc0df8dae8947e8665d
LoryBug <72506393+LoryBug@users.noreply.github.com>
2023-01-29 16:58:33 +0100

    fix bug comment

--- template/base-myprofile.php
+++ template/base-myprofile.php
@@ -20,7 +20,38 @@
 </head>
 
 <body class="bg-white">
-    <?php require_once("nav.php"); ?>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+                <h3>Socialtrack</h3>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="index.php">Home <span class="bi bi-fuel-pump"></span> </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="track.php">Track <span class="bi bi-map"></span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="myprofile.php">My Account <span
+                                class="bi bi-person"></span></a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-light" type="submit"><span class="bi bi-search"></span></button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
 
     <div class="container text-center border shadow mt-4 pt-4">
         <div class="container">
@@ -81,8 +112,12 @@
             </div>
         </div>
     </div>
-    <?php require_once("footer.php"); ?>
-
+    <footer class="mt-5 p-4 bg-dark text-white text-center pt-3">
+        <a href="#" class="text-decoration-none link-light">
+            <h3>SocialTrack</h3>
+        </a>
+        <p class="text-center text-muted">© 2023 Socialtrack by Leoni, Casamenti</p>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>

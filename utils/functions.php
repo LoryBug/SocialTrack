6651dbb02a5c8e46516d6d1dd9cc77fec1325a64
LoryBug <72506393+LoryBug@users.noreply.github.com>
2023-01-30 16:00:06 +0100

    add function

--- utils/functions.php
+++ utils/functions.php
@@ -6,5 +6,14 @@ function registerLoggedUser($user){
 function isUserLoggedIn(){
     return !empty($_SESSION["username"]);
 }
+// transform a multidimensional array into a single array
+function multiDimArrayToArray($array){
+    $result = array();
+    foreach($array as $user){
+        array_push($result, $user["FOL_Username"]);
+    }
+    return $result;
+}
+
 
 ?>

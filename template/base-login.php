6628d0ede6b0a37725573d1ff75a2e3fd812a1a8
gianmaria casamenti <gianmaria.casamenti@studio.unibo.it>
2023-02-13 21:02:39 +0100

    validate html

--- template/base-login.php
+++ template/base-login.php
@@ -25,8 +25,8 @@
         <div class="text center text-lg-start mt-lg-5 pt-lg-5">
             <h1 class="fw-bold fs-0 ">Social Track</h1>
             <p class="w-75 mx-auto mx-lg-0 fs-4 text-primary">Il Social Network nato per i biker e ideato da biker</p>
-            <img class= "d-none d-sm-block" src="upload/moto-login 2.png" alt="icon-socialtrack" target="redMoto">
-            <img class= "d-none d-sm-block" src="upload/moto-login.png" alt="icon-socialtrack" target="blueMoto">
+            <img class= "d-none d-sm-block redMoto" src="upload/moto-login2.png" alt="icon-socialtrack" >
+            <img class= "d-none d-sm-block blueMoto" src="upload/moto-login.png" alt="icon-socialtrack" >
 
         </div>
         <!--form-->
